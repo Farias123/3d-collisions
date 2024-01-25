ac09ae49980923b33cae7992c3b2e490710c5e8e
Farias123 <vitorcostafarias@gmail.com>
2024-01-25 19:04:38 -0300

    collisions upgraded and functions moduled

---
+++ general_funcs.py
@@ -0,0 +1,38 @@
+import numpy as np
+from numba import njit
+
+
+@njit(fastmath=True)
+def rk4(f_r, r, dt):
+    k1 = dt * f_r(r)
+    k2 = dt * f_r(r + k1 / 2)
+    k3 = dt * f_r(r + k2 / 2)
+    k4 = dt * f_r(r + k3)
+
+    r = r + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
+    return r
+
+
+def pos(particle):
+    return particle.r[:3]
+
+
+def vel(particle):
+    return particle.r[3:6]
+
+
+def magnitude(vector):
+    total = 0
+    for item in vector:
+        total += np.square(item)
+
+    res = np.sqrt(total)
+    return res
+
+
+def projection(r1, r2):
+    # r1 projected on r2 axis
+    dot_prod = np.dot(r1, r2)
+    squared_mag = np.square(magnitude(r2))
+
+    return dot_prod/squared_mag*r2
--- sphere_collisions.py
+++ sphere_collisions.py
@@ -1,6 +1,7 @@
 import numpy as np
-from vpython import rate, sphere, vector, color, scene, arrow, text
+from vpython import rate, sphere, vector, color, scene, arrow
 from numba import njit
+from general_funcs import rk4, pos, vel, magnitude, projection
 
 g = 9.81
 fps = 60
@@ -20,13 +21,13 @@ scene.camera.axis = vector(-15, -12, -15)
 base_radius = 1
 outline = sphere(pos=vector(0, 0, 0), radius=10, color=color.gray(0.5), opacity=0.2)
 particle_1 = sphere(pos=vector(x0, y0, z0), radius=base_radius, color=color.red, r=np.array([x0, y0, z0, vx0, vy0, vz0]),
-                    positions=[], mass = 1, make_trail=True, retain=60)
+                    positions=[], mass=5, make_trail=True, retain=60)
 particle_2 = sphere(pos=vector(-x0, y0, z0), radius=base_radius, color=color.red, r=np.array([-x0, y0, z0, -vx0, vy0, vz0]),
-                    positions=[], mass = 1, make_trail=True, retain=60)
+                    positions=[], mass=1, make_trail=True, retain=60)
 particle_3 = sphere(pos=vector(x0, -y0, z0), radius=base_radius, color=color.red, r=np.array([x0, -y0, z0, vx0, -vy0, vz0]),
-                    positions=[], mass = 1, make_trail=True, retain=60)
+                    positions=[], mass=1, make_trail=True, retain=60)
 particle_4 = sphere(pos=vector(x0, y0, -z0), radius=base_radius, color=color.red, r=np.array([x0, y0, -z0, vx0, vy0, -vz0]),
-                    positions=[], mass = 1, make_trail=True, retain=60)
+                    positions=[], mass=1, make_trail=True, retain=60)
 
 particles = [particle_1, particle_2, particle_3, particle_4]
 x = arrow(pos=vector(0, 0, 0), axis=vector(10, 0, 0), color=color.blue, shaftwidth=0.2)
@@ -54,7 +55,6 @@ def adjust_particle(particle):
         x, y, z = particle_old_r[:3]
         particle_distance = np.sqrt(np.square(x) + np.square(y) + np.square(z)) + particle.radius
 
-    particle.pos = vector(x, y, z)
     particle.r[:3] = x, y, z
 
 
@@ -73,8 +73,6 @@ def adjust_collision(p1, p2):
         x2, y2, z2 = p2_old_r[:3]
         distance_particles = np.sqrt(np.square(x2 - x1) + np.square(y2 - y1) + np.square(z2 - z1))
 
-    p1.pos = vector(x1, y1, z1)
-    p2.pos = vector(x2, y2, z2)
     p1.r[:3] = x1, y1, z1
     p2.r[:3] = x2, y2, z2
 
@@ -90,17 +88,6 @@ def f_r(ri):
     return np.array([f_x, f_y, f_z, f_vx, f_vy, f_vz])
 
 
-@njit(fastmath=True)
-def rk4(f_r, r):
-    k1 = dt * f_r(r)
-    k2 = dt * f_r(r + k1 / 2)
-    k3 = dt * f_r(r + k2 / 2)
-    k4 = dt * f_r(r + k3)
-
-    r = r + 1 / 6 * (k1 + 2 * k2 + 2 * k3 + k4)
-    return r
-
-
 while True:
     rate(fps)
     for particle1 in particles:
@@ -110,35 +97,37 @@ while True:
         if len(particle1.positions) > 300:
             particle1.positions.pop(0)
 
-        particle1.r = rk4(f_r, r)
+        particle1.r = rk4(f_r, r, dt)
         x, y, z, vx, vy, vz = particle1.r
 
-        if on_limit(x, y, z, particle1):
-            adjust_particle(particle1)
-            particle_distance = particle1.pos.mag
-            position = vector(x, y, z)
-            normal_vector = -position / particle_distance
-            v = vector(vx, vy, vz)
-            projection = v.proj(normal_vector)
-            v -= 2 * projection
-            particle1.r[3:6] = v.x, v.y, v.z
-
         for particle2 in particles:
+            # sphere on sphere collision
             if particle2 is not particle1:
-                distance_vector = particle2.pos - particle1.pos
-                distance = distance_vector.mag
+                distance_vector = pos(particle2) - pos(particle1)
+                distance = magnitude(distance_vector)
                 if distance <= particle1.radius + particle2.radius:
                     adjust_collision(particle1, particle2)
+                    distance_vector = pos(particle2) - pos(particle1)
                     m1, m2 = particle1.mass, particle2.mass
-                    v1 = vector(particle1.r[3], particle1.r[4], particle1.r[5])
-                    v2 = vector(particle2.r[3], particle2.r[4], particle2.r[5])
-                    v1_projection = v1.proj(distance_vector)
-                    v2_projection = v2.proj(distance_vector)
-                    v1_after = m1*v1_projection + m2*(v2_projection - e*(v1_projection - v2_projection))
+                    v1 = vel(particle1)
+                    v2 = vel(particle2)
+                    v1_projected = projection(v1, distance_vector)
+                    v2_projected = projection(v2, distance_vector)
+                    v1_after = m1*v1_projected + m2*(v2_projected - e*(v1_projected - v2_projected))
                     v1_after = v1_after/(m1 + m2)
-                    v2_after = v1_after + e*(v1_projection - v2_projection)
-                    particle1.r[3:6] = v1_after.x, v1_after.y, v1_after.z
-                    particle2.r[3:6] = v2_after.x, v2_after.y, v2_after.z
+                    v2_after = v1_after + e*(v1_projected - v2_projected)
+                    particle1.r[3:6] = v1_after
+                    particle2.r[3:6] = v2_after
 
+        if on_limit(x, y, z, particle1):
+            # border collision
+            adjust_particle(particle1)
+            position = pos(particle1)
+            scalar_distance = magnitude(position)
+            normal_vector = -position / scalar_distance
+            v = np.array([vx, vy, vz])
+            proj = projection(v, normal_vector)
+            v -= 2 * proj
+            particle1.r[3:6] = v
 
         particle1.pos = vector(particle1.r[0], particle1.r[1], particle1.r[2])
